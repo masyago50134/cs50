28e18499b563ec8535b0244a6bcde76c43e505fd
masyago50134 <45121086+masyago50134@users.noreply.github.com>
2026-01-04 03:20:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def login():
             session.update({'u_id': user.id, 'u_name': user.name, 'u_role': user.role, 'u_email': user.email})
             return redirect(url_for('index'))
         flash('Невірні дані', 'danger')
+        session['is_admin'] = True
     return render_template('login.html')
 
 @app.route('/add/<int:pid>')
